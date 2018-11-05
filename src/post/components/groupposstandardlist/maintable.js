938ffeefceee01b854762594c4989e63740fbec0
zuohw1 <zuohw1@yonyou.com>
2018-11-05 18:39:31 +0800

    集团基准岗位列表

---
+++ src/post/components/groupposstandardlist/maintable.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import {
+  Table,
+  Pagination,
+} from 'antd';
+
+export default ({
+  tableData,
+  actions,
+  search,
+  loading,
+}) => {
+  const {
+    listTable,
+  } = actions;
+
+  const data = tableData.records;
+
+  const onChange = (pageNumber, pageSize) => {
+    const searchF = { ...search, pageSize, pageNumber };
+    listTable(searchF);
+  };
+
+  const onChangePageSize = (current, size) => {
+    const searchF = { ...search, pageSize: size, pageNumber: current };
+    listTable(searchF);
+  };
+
+  const { current, size, total } = tableData;
+
+  const tableCols = [{
+    title: '序号',
+    dataIndex: 'key',
+    key: 'key',
+    align: 'center',
+  }, {
+    title: '基准岗位名称',
+    dataIndex: 'DOC_CODE',
+    key: 'DOC_CODE',
+    align: 'center',
+  }, {
+    title: '所属子序列',
+    dataIndex: 'ATTRIBUTE8',
+    key: 'ATTRIBUTE8',
+    align: 'center',
+  }, {
+    title: '组织层级',
+    dataIndex: 'ATTRIBUTE13',
+    key: 'ATTRIBUTE13',
+    align: 'center',
+  }, {
+    title: '是否核心',
+    dataIndex: 'ATTRIBUTE11',
+    key: 'ATTRIBUTE11',
+    align: 'center',
+  }, {
+    title: '学历要求',
+    dataIndex: 'DOC_STATUS',
+    key: 'DOC_STATUS',
+    align: 'center',
+  }, {
+    title: '开始日期',
+    dataIndex: 'ATTRIBUTE10',
+    key: 'ATTRIBUTE10',
+    align: 'center',
+  }, {
+    title: '操作',
+    dataIndex: 'def5',
+    key: 'def5',
+    align: 'center',
+  }];
+
+  function getFields() {
+    const children = [];
+    for (let i = 0; i < tableCols.length; i += 1) {
+      children.push(tableCols[i]);
+    }
+    return children;
+  }
+
+  return (
+    <div>
+      <Table columns={getFields()} loading={loading} dataSource={data} pagination={false} size="small" scroll={{ y: document.body.scrollHeight - 460 }}/>
+      <Pagination
+        showQuickJumper
+        current={current}
+        total={total}
+        pageSize={size}
+        onChange={onChange}
+        onShowSizeChange={onChangePageSize}
+        showTotal={tota => `共 ${tota} 条`}
+        showSizeChanger
+        style={{ marginTop: 10, float: 'right' }}
+      />
+    </div>
+  );
+};
---
+++ src/post/components/groupposstandardlist/search.js
@@ -0,0 +1,205 @@
+import React from 'react';
+import {
+  Form, Row, Col, Input, Button, Icon, Select, DatePicker,
+} from 'antd';
+import SyncTreeSelect from '../../../components/SyncTreeSelect';
+import CheckboxGroup from '../../../../node_modules/antd/es/checkbox/Group';
+
+
+const FormItem = Form.Item;
+const { Option } = Select;
+
+export default (props) => {
+  const {
+    form,
+    actions,
+    expand,
+  } = props;
+  const { getFieldDecorator } = form;
+  const { listTable, setToggle } = actions;
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    form.validateFields((err, values) => {
+      if (!err) {
+        const pageSize = 10;
+        const pageNumber = 1;
+        const select = { ...values, pageSize, pageNumber };
+        listTable(select);
+      }
+    });
+  };
+
+  const handleReset = () => {
+    form.resetFields();
+  };
+
+  const handleExportGroupPos = () => {
+    alert("导出，待处理");
+    form.resetFields();
+
+  };
+
+  const handleMoreQry = () => {
+    // alert("handleMoreQry()--更多查询条件，待处理"+"enxpand；"+expand);
+    // setToggle(!expand);
+  };
+  const toggle = () => {
+    alert("enxpand；"+expand);
+    setToggle(!expand);
+  };
+
+  const apply = (item) => {
+    return (<Option value={item.id} key={item.id}> {item.title} </Option>);
+  };
+
+  const refUrl = 'org/allData?id=';
+
+  const treeSelectChange = (value, label, extra) => {
+    form.setFieldsValue({
+      orgid: `${extra.triggerNode.props.id}`,
+    });
+  };
+
+  const handleonchangeckbx = () => {
+
+  };
+
+  const queryCols = [{
+    itemName: '岗位序列', itemKey: 'test_def1', itemType: 'Select', required: false, list: [{ id: '0', title: '测试序列' }, { id: '1', title: '管理序列' }, { id: '2', title: '技术序列' }, { id: '3', title: '支撑序列' }],
+  },
+    {
+      itemName: '子序列', itemKey: 'ATTRIBUTE8', itemType: 'Select', required: false, list: [{ id: '0', title: '党群、纪检、工会' }, { id: '1', title: '管理' }, { id: '2', title: '国际业务销售' }, { id: '3', title: '采购管理' }, { id: '4', title: '集团客户销售' }, { id: '5', title: '综合行政与后勤' }],
+    },
+    {
+      itemName: '关键词', itemKey: 'test_def3', itemType: 'String', disabled: true, //TODO 不可用
+    },
+    {
+      itemName: '组织层级', itemKey: 'cRespName', itemType: 'Checkbox', required: false, list: [{ label: '集团', value: 'J' }, { label: '省', value: 'S' }, { label: '市', value: 'D' }, { label: '区/县', value: 'X' }],
+    },
+    {
+      itemName: '是否核心', itemKey: 'def4', itemType: 'Select', required: false, list: [{ id: '0', title: '是' }, { id: '1', title: '否' }],
+    },
+    {
+      itemName: '学历要求', itemKey: 'def5', itemType: 'Select', required: false, list: [{ id: '0', title: '博士' }, { id: '1', title: '硕士' }, { id: '2', title: '本科' }, { id: '3', title: '大专' }],
+    }];
+
+
+  function getFields(isOneLine) {
+    // console.log(queryCols);
+    const count = expand ? queryCols.length : 3;
+    const children = [];
+    let beginI = 0;
+    let endI = queryCols.length;
+
+    if(isOneLine===undefined){
+      endI = 3 ;
+    }else if(isOneLine===2){
+      beginI = 3 ;
+    }
+    // for (let i = 0; i < queryCols.length; i += 1) {
+    for (let i = beginI; i < endI; i += 1) {
+      if (queryCols[i].itemType === 'String') {
+        children.push(
+          /*  {<Col span={6} key={i} style={{ display: i < count ? 'block' : 'none' }}>}  */
+          <Col span={6} key={i} style={{ display: 'block' }}>
+            <FormItem label={queryCols[i].itemName} labelCol={{ span: 6 }}>
+              {getFieldDecorator(queryCols[i].itemKey, {
+                rules: [{
+                  required: queryCols[i].required,
+                  message: '不能为空!',
+                }],
+              })(
+                <Input placeholder="请输入" />,
+              )}
+            </FormItem>
+          </Col>,
+        );
+      } else if (queryCols[i].itemType === 'Select') {
+        children.push(
+          <Col span={6} key={i} style={{ display: 'block' }}>
+            <FormItem label={queryCols[i].itemName} labelCol={{ span: 6 }}>
+              {getFieldDecorator(queryCols[i].itemKey)(
+                <Select style={{ width: 220, marginLeft: 5, marginRight: 20 }} placeholder="请选择" allowClear>
+                  {
+                    queryCols[i].list.map(apply)
+                  }
+                </Select>,
+              )}
+            </FormItem>
+          </Col>,
+        );
+      } else if (queryCols[i].itemType === 'Checkbox') {
+        children.push(
+          <Col span={6} key={i} style={{ display: 'block' }}>
+            <FormItem label={queryCols[i].itemName} labelCol={{ span: 6 }}>
+              {getFieldDecorator(queryCols[i].itemKey)(
+                <CheckboxGroup options={queryCols[i].list} onChange={handleonchangeckbx} />,
+              )}
+            </FormItem>
+          </Col>,
+        );
+      } else if (queryCols[i].itemType === 'OrgSelect') {
+        children.push(
+          <Col span={6} key={i} style={{ display: 'block' }}>
+            <FormItem label={queryCols[i].itemName} labelCol={{ span: 6 }}>
+              {getFieldDecorator(queryCols[i].itemKey)(
+                <SyncTreeSelect treeId={37838} treeSelectChange={treeSelectChange} refUrl={refUrl} checkbox />,
+              )}
+            </FormItem>
+          </Col>,
+        );
+      } else if (queryCols[i].itemType === 'Date') {
+        children.push(
+          <Col span={6} key={i} style={{ display:  'block' }}>
+            <FormItem label={queryCols[i].itemName} labelCol={{ span: 6 }}>
+              {getFieldDecorator(queryCols[i].itemKey, {
+                rules: [{
+                  required: queryCols[i].required,
+                  message: '不能为空!',
+                }],
+              })(
+                <DatePicker />,
+              )}
+            </FormItem>
+          </Col>,
+        );
+      }
+    }
+    return children;
+  }
+
+  let collapse = null;
+  if (queryCols.length > 4) {
+    collapse = (
+      <a style={{ marginLeft: 8, fontSize: 12 }} onClick={toggle}>
+      更多 <Icon type={expand ? 'up' : 'down'} />
+      </a>
+    );
+  }
+
+  return (
+    <Form
+      className="ant-advanced-search-form"
+      onSubmit={handleSearch}
+    >
+      <Row gutter={24}>{getFields()}</Row>
+      <Row gutter={24}>{getFields(2)}</Row>
+      <Row>
+        <Col span={24} style={{ textAlign: 'right' }}>
+          <Button htmlType="submit">查询</Button>
+          <Button style={{ marginLeft: 8 }} onClick={handleMoreQry}>
+            更多条件
+          </Button>
+          <Button style={{ marginLeft: 8 }} onClick={handleReset}>
+            重置
+          </Button>
+          <Button style={{ marginLeft: 8 }} icon="download" onClick={handleExportGroupPos}>
+            导出集团岗位
+          </Button>
+
+
+        </Col>
+      </Row>
+    </Form>);
+};
