dbb65a50a591227ed9e5a0554135d85b2c0b0388
zuohw1 <zuohw1@yonyou.com>
2018-11-09 15:07:06 +0800

    代码规范

--- src/post/containers/key-resp-rank.js
+++ src/post/containers/key-resp-rank.js
@@ -1,13 +1,13 @@
 import { connect } from 'dva';
 import { bindActionCreators } from 'redux';
 import { Form } from 'antd';
-import Index from '../components/groupposstandardlist/index';
-import * as actions from '../actions/groupposstandardlist';
+import Index from '../components/keyresprank/index';
+import * as actions from '../actions/key-resp-rank';
 
 /* 建立组件跟 store 的 state 的映射关系 */
 const mapStateToProps = state => ({
-  ...state.groupPosStandardList,
-  loading: state.loading.models.groupPosStandardList,
+  ...state.keyRespRank,
+  loading: state.loading.models.keyRespRank,
 });
 
 const WrappedApp = Form.create()(Index);
