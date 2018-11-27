a4083d1fc98b6629d4f8f8495be19c70c707cdee
ly956428904 <16679086372@163.com>
2018-11-27 09:30:05 +0800

    管理人员职位管理(集团)---搜索调整

--- src/post/components/managepostgroup/index.js
+++ src/post/components/managepostgroup/index.js
@@ -14,7 +14,7 @@ const ManagePostGroup = (state) => {
           职位管理
         </Breadcrumb.Item>
         <Breadcrumb.Item>
-          <strong>管理人员职位管理(集团)</strong>
+          <strong>管理人员职位管理（集团）</strong>
         </Breadcrumb.Item>
       </Breadcrumb>
       <Content
