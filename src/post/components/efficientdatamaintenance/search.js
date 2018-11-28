457ad761cba4db62a025b6ee3015e732b8b5ae58
zuohw1 <zuohw1@yonyou.com>
2018-11-28 11:11:18 +0800

    效率业务数据维护-组织的树状显示

--- src/post/components/efficientdatamaintenance/search.js
+++ src/post/components/efficientdatamaintenance/search.js
@@ -1,18 +1,19 @@
+/* eslint-disable max-len */
 import React from 'react';
 import {
-  Form, Row, Col, Input, Button, Select, DatePicker,
+  Form, Row, Col, Button, Select, DatePicker, Tree, TreeSelect,
 } from 'antd';
-import SyncTreeSelect from '../../../components/sync-tree-select';
 
 const FormItem = Form.Item;
 const { Option } = Select;
 const { RangePicker } = DatePicker;
-export default (props) => {
+const { TreeNode } = Tree;
+export default (state) => {
   const {
     form,
     actions,
     expand,
-  } = props;
+  } = state;
   const { getFieldDecorator } = form;
   const { listTable } = actions;
 
@@ -39,33 +40,62 @@ export default (props) => {
       orgid: `${extra.triggerNode.props.id}`,
     });
   };
+  const renderTreeNodes = () => {
+    // return data.map((item) => {
+    //   if (item.children) {
+    //     return (
+    //       <TreeNode icon={<Icon type="star-o" theme="twoTone" />} title={item.title} key={item.key} dataRef={item}>
+    //         {renderTreeNodes(item.children)}
+    //       </TreeNode>
+    //     );
+    //   }
+    // });
+    return (
+      // <TreeNode {...item} dataRef={item} />;
+      <TreeNode value="parent" title="中国联合网络通信集团有限公司" key="0-1">
+        <TreeNode value="parent 1-0" title="中国联通总部管理部门" key="0-1-1">
+          <TreeNode value="parent 1-1" title="中国联通总部-综合部（董事会办公室）" key="0-1-1-1">
+            <TreeNode value="leaf1" title="中国联通总部-综合部" key="random" />
+            <TreeNode value="leaf2" title="中国联通总部-综合部" key="random1" />
+          </TreeNode>
+        </TreeNode>
+        <TreeNode value="parent 1-1" title="中国联通总部-办公厅" key="random2">
+          <TreeNode value="sss" title="中国联通总部-办公厅" key="random3" />
+        </TreeNode>
+      </TreeNode>
+    );
+  };
+
   /* 查询字段 */
   const queryCols = [{
-    itemName: '组织', itemKey: 'test_def1', itemType: 'Select', required: false, list: [{ id: '0', title: '中国联合网络集团通信有限公司' }, { id: '1', title: '省分公司' }],
+    itemName: '组织', itemKey: 'test_def1', itemType: 'OrgSelect', required: false,
   },
   {
     itemName: '期间', itemKey: 'ATTRIBUTE8', itemType: 'Date', required: false,
   },
   {
-    itemName: '效率指标', itemKey: 'test_def3', itemType: 'String', required: false,
+    itemName: '效率指标', itemKey: 'test_def3', itemType: 'Select', required: false, list: [{ id: '0', title: '市场营销策划专业效率' }, { id: '1', title: '市场业务管理专业效率' }, { id: '2', title: '产品管理专业效率' }, { id: '3', title: '合作业务管理专业效率' }],
   }];
-
   function getFields() {
     const count = expand ? queryCols.length : 3;
     const children = [];
 
     for (let i = 0; i < queryCols.length; i += 1) {
-      if (queryCols[i].itemType === 'String') {
+      if (queryCols[i].itemType === 'OrgSelect') {
         children.push(
-          <Col span={6} key={i} style={{ display: i < count ? 'block' : 'none' }}>
+          <Col span={6} key={i} style={{ display: 'block' }}>
             <FormItem label={queryCols[i].itemName} labelCol={{ span: 6 }}>
-              {getFieldDecorator(queryCols[i].itemKey, {
-                rules: [{
-                  required: queryCols[i].required,
-                  message: '不能为空!',
-                }],
-              })(
-                <Input placeholder="请输入" style={{ width: 220, marginLeft: 5, marginRight: 20 }} />,
+              {getFieldDecorator(queryCols[i].itemKey)(
+                <TreeSelect
+                  treeId={37838}
+                  treeSelectChange={treeSelectChange}
+                  refUrl={refUrl}
+                  placeholder="请选择"
+                  allowClear
+                  treeDefaultExpandAll
+                >
+                  {renderTreeNodes(state.orgTree)}
+                </TreeSelect>,
               )}
             </FormItem>
           </Col>,
@@ -80,21 +110,6 @@ export default (props) => {
                     queryCols[i].list.map(apply)
                   }
                 </Select>,
-              )}
-            </FormItem>
-          </Col>,
-        );
-      } else if (queryCols[i].itemType === 'OrgSelect') {
-        children.push(
-          <Col span={6} key={i} style={{ display: 'block' }}>
-            <FormItem label={queryCols[i].itemName} labelCol={{ span: 6 }}>
-              {getFieldDecorator(queryCols[i].itemKey)(
-                <SyncTreeSelect
-                  treeId={37838}
-                  treeSelectChange={treeSelectChange}
-                  refUrl={refUrl}
-                  checkbox
-                />,
               )}
             </FormItem>
           </Col>,
